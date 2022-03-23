b34671f0e531e947dfbe14fc6e8dd297f3383e2d
Shakia Tasnim <99421796+Shakia123@users.noreply.github.com>
2022-03-24 01:23:58 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+
+    $('#second').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    }),
+    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+});
+
+$(document).ready(function(){
+
+    $('#hotel-form').show();
+    $('#ticket-form').hide();
+    $('#place-form').hide();
+
+
+    $('#hotel-item span').addClass('line');
+
+    //click on ticket
+    $('#ticket-item').click(function(){
+
+        $('#hotel-form').hide();
+        $('#ticket-form').show();
+        $('#place-form').hide();
+
+        $('#hotel-item span').removeClass('line');
+        $('#ticket-item span').addClass('line');
+        $('#place-item span').removeClass('line');
+    });
+
+    //click on place
+    $('#place-item').click(function(){
+
+        $('#hotel-form').hide();
+        $('#ticket-form').hide();
+        $('#place-form').show();
+
+        $('#hotel-item span').removeClass('line');
+        $('#ticket-item span').removeClass('line');
+        $('#place-item span').addClass('line');
+    });
+
+    //click on hotel
+    $('#hotel-item').click(function(){
+
+        $('#hotel-form').show();
+        $('#ticket-form').hide();
+        $('#place-form').hide();
+
+        $('#hotel-item span').addClass('line');
+        $('#ticket-item span').removeClass('line');
+        $('#place-item span').removeClass('line');
+    });
+
+})
